b75f1e2af731aa2387af596cf66f73fbf0fe887c
Picori <picori@163.com>
2011-07-17 21:17:03 +0800

    rename resource directory

--- content/overlay.js
+++ content/overlay.js
@@ -1,18 +1,24 @@
 Components.utils.import("resource://gre/modules/PopupNotifications.jsm");
-Components.utils.import("resource://resource/Affiliate.jsm");
-var RebateRobot = {
+Components.utils.import("resource://modules/util.js");
+Components.utils.import("resource://modules/Affiliate.jsm");
+var aConsoleService = Utils.getService("@mozilla.org/consoleservice;1","nsIConsoleService");
+var RebateRobot = RebateRobot || {
     onLoad: function() {
         // initialization code
+        this.toggleTabsProgressListener(Utils.preference("enable"));
         this.initialized = true;
-        gBrowser.addTabsProgressListener({
-            onLocationChange:function(aBrowser,webProgress,request,newLocation){
-                Affiliate.distribute(aBrowser,webProgress,request,newLocation,PopupNotifications);
-            },
-            onProgressChange:function(){},
-            onSecurityChange:function(){},
-            onStateChange:function(){}
-        });
+    },
+    listener:{
+        onLocationChange:function(aBrowser,webProgress,request,newLocation){
+            Affiliate.distribute(aBrowser,webProgress,request,newLocation,PopupNotifications);
+        },
+        onProgressChange:function(){},
+        onSecurityChange:function(){},
+        onStateChange:function(){}
+    },
+    toggleTabsProgressListener:function(status){
+        gBrowser[(status ?"add":"remove")+"TabsProgressListener"](this.listener);
     }
 };
-
+//
 window.addEventListener("load", function(e) {RebateRobot.onLoad(e);}, true); 
